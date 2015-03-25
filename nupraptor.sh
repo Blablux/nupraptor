45031def9b6197868b9eca1ace47c8a5f8c74ac8
Benoit Hetru <hetrube@gmail.com>
2015-03-25 14:36:05 +0100

    properly create result files

--- nupraptor.sh
+++ nupraptor.sh
@@ -11,25 +11,31 @@ function senderror()
  exit 1 ;
 }
 
-scriptpath=`dirname $0`
+scriptpath=$(dirname "$0")
 # check if file exist and is XML
-if [ ! -z $1 ]; then
-  filepath=`dirname $1`
+if [ ! -z "$1" ]; then
+  filepath=$(dirname "$1")
   filename=$(basename "$1")
   ext="${filename##*.}"
   filename="${filename%.*}"
-  workingname="$scriptpath"/temp/"$filename"
-  if [ ! -r $filepath/$filename.$ext ]; then
+  if [ ! -r "$filepath"/"$filename"."$ext" ]; then
     senderror "$filename.$ext can't be read!"
   fi
   if [ "$ext" != "xml" ]; then
-    senderror "$filename.$ext is not a XBMC/Kodi database file!"
+    senderror "$filename.$ext is not a XBMC/Kodi database export file!"
   fi
 else
   senderror "You must supply the file to convert"
 fi
 
-mkdir -p $scriptpath/temp
+mkdir -p "$scriptpath"/temp
 
-xsltproc movie.xsl $1 > $scriptpath/temp/movies.csv
-xsltproc series.xsl $1 > $scriptpath/temp/series.csv
+echo "Processing movies..."; 
+xsltproc movie.xsl "$1" > "$scriptpath"/temp/movies.csv
+echo "Processing series..."; 
+xsltproc series.xsl "$1" > "$scriptpath"/temp/series.csv
+
+cp "$scriptpath"/temp/*.csv "$filepath"
+rm -r "$scriptpath"/temp/
+echo "Files written in $filepath";
+
